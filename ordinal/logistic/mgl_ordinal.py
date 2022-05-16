004cfa06d53cff48932eb9ed88ad510af2f52489
soyeungjo <ok69531@gmail.com>
2022-05-16 21:04:51 +0900

    ordinal logistic

--- ordinal/logistic/mgl_ordinal.py
+++ ordinal/logistic/mgl_ordinal.py
@@ -145,6 +145,6 @@ def mgl_ordinal_main(seed_):
 result = []
 for seed_ in range(200):
       result.append(mgl_ordinal_main(seed_))
-      
-pd.DataFrame(result).to_csv('test_result/mgl_ordinal.csv', header = True, index = False)
+
+pd.DataFrame(result).to_csv('../test_result/mgl_ordinal.csv', header = True, index = False)
 wandb.finish()
