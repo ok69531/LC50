257e3985c10afeb6af51c74343d620e68027dc82
soyeungjo <ok69531@gmail.com>
2022-05-16 21:05:35 +0900

    ordinal logistic

--- ordinal/logistic/ppm_ordinal.py
+++ ordinal/logistic/ppm_ordinal.py
@@ -129,5 +129,5 @@ result = []
 for seed_ in range(200):
       result.append(ppm_ordinal_main(seed_))
       
-pd.DataFrame(result).to_csv('test_result/ppm_ordinal.csv', header = True, index = False)
+pd.DataFrame(result).to_csv('../test_result/ppm_ordinal.csv', header = True, index = False)
 wandb.finish()
