19fa52f98f336ffd75b5762262d4c7ed9b659a8b
soyeungjo <ok69531@gmail.com>
2022-05-16 22:35:13 +0900

    feat - reconstruction

--- ordinal/logistic/mgl_ordinal.py
+++ ordinal/logistic/mgl_ordinal.py
@@ -146,5 +146,5 @@ result = []
 for seed_ in range(200):
       result.append(mgl_ordinal_main(seed_))
 
-pd.DataFrame(result).to_csv('../test_result/mgl_ordinal.csv', header = True, index = False)
+pd.DataFrame(result).to_csv('../test_results/mgl_ordinal.csv', header = True, index = False)
 wandb.finish()
--- ordinal/logistic/ppm_ordinal.py
+++ ordinal/logistic/ppm_ordinal.py
@@ -130,5 +130,5 @@ result = []
 for seed_ in range(200):
       result.append(ppm_ordinal_main(seed_))
       
-pd.DataFrame(result).to_csv('../test_result/ppm_ordinal.csv', header = True, index = False)
+pd.DataFrame(result).to_csv('../test_results/ppm_ordinal.csv', header = True, index = False)
 wandb.finish()
